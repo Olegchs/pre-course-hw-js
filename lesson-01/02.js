0f24ac0ee984f091735037176b00850c4fb4ef5f
Oleg <oneofthemoon75@gmail.com>
2024-09-11 20:35:21 +0300

    test 2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,4 +1,4 @@
 let number = Math.floor(Math.random() * 100)
 
-console.log(number % 2 === 0 ? `${number} Это четное число` : `${number} Это нечетное число`)
+console.log(number % 2 === 0 ? `${number} :Это четное число` : `${number} :Это нечетное число`)
 //hey
